a07dc6864a4d461e852a7e57b7948eb3e59633b2
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-07-25 14:27:55 -0700

    added code comments

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,4 +1,6 @@
+// load Express router
 const router = require('express').Router();
+// load required Routes
 const userRoutes = require('./user-routes');
 const thoughtRoutes = require('./thought-routes');
 
