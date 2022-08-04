4251a003cf8db50091528dac7c2c1efe26db4699
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-08-03 22:20:25 -0700

    added many comments

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,8 +1,18 @@
+//*
+//* Controller for Thoughts
+//*
+
+// import the required Models
 const { Thought, User } = require('../models');
 
+// define the controller functions for the Thoughts routes
 const thoughtController = {
-  // get all thoughts
+  // GET for all thoughts
+  // API GET route is
   getThoughts(req, res) {
+    // Mongoose 'find()' query to return all documents
+    // The Mongoose 'sort()' method sets the sort order
+    // If an { object } is passed, values allowed are asc, desc, ascending, descending, 1, and -1
     Thought.find()
       .sort({ createdAt: -1 })
       .then((dbThoughtData) => {
@@ -10,13 +20,17 @@ const thoughtController = {
       })
       .catch((err) => {
         console.log(err);
+        // returns a "500 Internal Server Error"
         res.status(500).json(err);
       });
   },
-  // get single thought by id
+  // GET for single thought by id
+  // API GET route is
   getSingleThought(req, res) {
+    // Mongoose helper action format is 'findOne(conditions)', finds one matching document
     Thought.findOne({ _id: req.params.thoughtId })
       .then((dbThoughtData) => {
+        // throws a "404 Not Found" if no id match
         if (!dbThoughtData) {
           return res.status(404).json({ message: 'No thought with this id!' });
         }
@@ -24,20 +38,26 @@ const thoughtController = {
       })
       .catch((err) => {
         console.log(err);
+        // returns a "500 Internal Server Error"
         res.status(500).json(err);
       });
   },
   // create a thought
+  // API POST route is 
   createThought(req, res) {
+    // Mongoose helper action format is 'create(document)', shortcut for saving one or more documents to the database
     Thought.create(req.body)
       .then((dbThoughtData) => {
         return User.findOneAndUpdate(
           { _id: req.body.userId },
+          // MongoDB The '$push' operator appends a specified value to an array
           { $push: { thoughts: dbThoughtData._id } },
+          // To return the document with modifications made on the update, use the 'new: true' option
           { new: true }
         );
       })
       .then((dbUserData) => {
+        // throws a "404 Not Found" if no User id match
         if (!dbUserData) {
           return res.status(404).json({ message: 'Thought created but no user with this id!' });
         }
@@ -46,13 +66,22 @@ const thoughtController = {
       })
       .catch((err) => {
         console.log(err);
+        // returns a "500 Internal Server Error"
         res.status(500).json(err);
       });
   },
-  // update thought
+  // update thought by id
+  // API PUT route is 
   updateThought(req, res) {
-    Thought.findOneAndUpdate({ _id: req.params.thoughtId }, { $set: req.body }, { runValidators: true, new: true })
+    // Mongoose helper action format is 'findOneAndUpdate(conditions, update, options)'
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId }, 
+      { $set: req.body },
+      // for runValidators option see https://mongoosejs.com/docs/validation.html#update-validators
+      // To return the document with modifications made on the update, use the 'new: true' option
+      { runValidators: true, new: true })
       .then((dbThoughtData) => {
+        // throws a "404 Not Found" if no Thought id match
         if (!dbThoughtData) {
           return res.status(404).json({ message: 'No thought with this id!' });
         }
@@ -60,44 +89,58 @@ const thoughtController = {
       })
       .catch((err) => {
         console.log(err);
+        // returns a "500 Internal Server Error"
         res.status(500).json(err);
       });
   },
-  // delete thought
+  // DELETE thought by id
+  // API route is 
   deleteThought(req, res) {
     Thought.findOneAndRemove({ _id: req.params.thoughtId })
       .then((dbThoughtData) => {
+         // throws a "404 Not Found" if no Thought id match
         if (!dbThoughtData) {
           return res.status(404).json({ message: 'No thought with this id!' });
         }
-
-        // remove thought id from user's `thoughts` field
+        //* remove thought id from user's `thoughts` field, is this right?
+        // Mongoose helper action format is 'findOneAndUpdate(conditions, update, options)'
         return User.findOneAndUpdate(
-          { thoughts: req.params.thoughtId },
+          //{ thoughts: req.params.thoughtId },
+          { username: deletedThought.username },
+          // MongoDB The '$pull' operator removes from an existing array all instances of a value
+          // or values that match a specified condition
           { $pull: { thoughts: req.params.thoughtId } },
+          // To return the document with modifications made on the update, use the 'new: true' option
           { new: true }
         );
       })
       .then((dbUserData) => {
+        // throws a "404 Not Found" if no user id match
         if (!dbUserData) {
-          return res.status(404).json({ message: 'Thought created but no user with this id!' });
+          return res.status(404).json({ message: 'No user with this id!' });
         }
         res.json({ message: 'Thought successfully deleted!' });
       })
       .catch((err) => {
         console.log(err);
+        // returns a "500 Internal Server Error"
         res.status(500).json(err);
       });
   },
-
-  // add a reaction to a thought
+  // add a reaction to a Thought
+  // API POST route is 
   addReaction(req, res) {
+    // Mongoose helper action format is 'findOneAndUpdate(conditions, update, options)'
     Thought.findOneAndUpdate(
       { _id: req.params.thoughtId },
+      // MongoDB: '$addToSet' returns an array of all unique values that results from applying an expression to each document in a group.
       { $addToSet: { reactions: req.body } },
+      // for runValidators option see https://mongoosejs.com/docs/validation.html#update-validators
+      // To return the document with modifications made on the update, use the 'new: true' option
       { runValidators: true, new: true }
     )
       .then((dbThoughtData) => {
+        // throws a "404 Not Found" if no Thought id match
         if (!dbThoughtData) {
           return res.status(404).json({ message: 'No thought with this id!' });
         }
@@ -105,17 +148,25 @@ const thoughtController = {
       })
       .catch((err) => {
         console.log(err);
+        // returns a "500 Internal Server Error"
         res.status(500).json(err);
       });
   },
-  // remove reaction from a thought
+  // delete Reaction (by id) from a Thought
+  // API DELETE route is
   removeReaction(req, res) {
+    // Mongoose helper action format is 'findOneAndUpdate(conditions, update, options)'
     Thought.findOneAndUpdate(
       { _id: req.params.thoughtId },
+      // this will remove specific reply from the array
+      // where Reaction id matches value of 'params.reactionId'
       { $pull: { reactions: { reactionId: req.params.reactionId } } },
+      // for runValidators see https://mongoosejs.com/docs/validation.html#update-validators
+      // To return the document with modifications made on the update, use the 'new: true' option
       { runValidators: true, new: true }
     )
       .then((dbThoughtData) => {
+        // throws a "404 Not Found" if no Thought id match
         if (!dbThoughtData) {
           return res.status(404).json({ message: 'No thought with this id!' });
         }
@@ -123,9 +174,11 @@ const thoughtController = {
       })
       .catch((err) => {
         console.log(err);
+        // returns a "500 Internal Server Error"
         res.status(500).json(err);
       });
   },
-};
+};// end controller
 
+// export all functions for use in thought-routes
 module.exports = thoughtController;
