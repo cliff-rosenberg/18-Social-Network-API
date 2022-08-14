3fee8a6e3fa4119e1827b8fc626e34c0f6092ad2
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-08-13 22:06:17 -0700

    added code comments

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,6 +1,8 @@
 const { Schema, Types } = require('mongoose');
+// load helper for date formatting
 const dateFormat = require('../utils/dateFormat');
 
+// Mongoose Note: "An ObjectId is a special type typically used for unique identifiers."
 const reactionSchema = new Schema(
   {
     reactionId: {
@@ -29,5 +31,10 @@ const reactionSchema = new Schema(
     id: false
   }
 );
+
+// NOTE about Getters/Setters in Mongoose schemas-
+// "Mongoose 'getters' and 'setters' allow you to execute custom logic when getting or setting a property on a Mongoose document."
+// "Getters let you transform data in MongoDB into a more user friendly form"
+// "Setters let you transform user data before it gets to MongoDB"
 
 module.exports = reactionSchema;
