5eb899383d50bacad936cb45e9313d2696522081
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-08-14 20:35:12 -0700

    updated code comments

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
-// load Express router setup
+// The express.Router class creates modular, mountable route handlers
+// A Router instance is a complete middleware and routing system;
+// for this reason, it is often referred to as a “mini-app”
+// see https://expressjs.com/en/guide/routing.html for more info
 const router = require('express').Router();
+// set up routes here
 const apiRoutes = require('./api');
 
+// the "API Routes" need the prefix of "/api" to work
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
